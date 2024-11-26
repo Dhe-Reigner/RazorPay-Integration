197f1aeb87e60a7b4dc18e63a0f4d64792e73fe8
Dhe-Reigner <martinkagema01@gmail.com>
2024-11-26 09:39:38 +0300

    Initial commit

--- DARAJA/asgi.py
+++ DARAJA/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RAZORPAY project.
+ASGI config for DARAJA project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RAZORPAY.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DARAJA.settings')
 
 application = get_asgi_application()
--- PaymentIntegration/views.py
+++ PaymentIntegration/views.py
@@ -1,8 +1,31 @@
 from django.shortcuts import render
+from . models import Coffee
+
+import razorpay
+
 
 # Create your views here.
-def home(request):
-    return render(request, 'index.html')
+# def home(request):
+#     all_mug = Coffee.objects.all()
+#     return render(request, 'index.html',{
+#         'mug':all_mug
+#     })
 
+
+# def home(request):
+#     if request.method == "POST":
+#         coffee_name = request.POST.get('coffee_name')
+#         coffee_amount = request.POST.get('coffee_amount')
+#         payment_id = request.POST.get('payment_id')
+#         Coffee.objects.create(name=coffee_name, amount=coffee_amount, payment_id=payment_id, paid=True)
+#         return render(request,'success.html')
+#     return render(request, 'home.html')
+
+def home(request):
+    if request.method  == 'POST':
+        name = request.POST.get('name')
+        amount = request.POST.get('amount')
+        client = razorpay.Client(auth = ())
+        print(name + amount)
 def success(request):
     return render(request, 'success.html')
